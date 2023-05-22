2962b058913f9dcf138ff03d9f9451a8462979d1
Ali <mohajer13sam@gmail.com>
2023-05-22 22:56:46 +0330

    controller

--- src/main/java/com/example/workflow/controller/Controller.java
+++ src/main/java/com/example/workflow/controller/Controller.java
@@ -1,9 +1,12 @@
 package com.example.workflow.controller;
 
+import com.example.workflow.model.*;
 import com.example.workflow.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
@@ -12,9 +15,34 @@ public class Controller {
     private TestService testService;
 
 
-    @GetMapping("/start")
-    public void start(){
-        testService.startProcess();
+    @GetMapping("/searchClaimableTasks")
+    public List<QuestionnaireInboxItem>  searchClaimableTasks(ClaimableTasksSearchParams params){
+        return testService.searchClaimableTasks(params);
+    }
+
+    @GetMapping("/myAssignedTasks")
+    public List<QuestionnaireInboxItem> myAssignedTasks(MyAssignedTasksSearchParams searchParams){
+        return testService.myAssignedTasks(searchParams);
+    }
+
+    @GetMapping("/myActiveInitiatedTasks")
+    public List<QuestionnaireInboxItem> myActiveInitiatedTasks(MyInitiatedSearchParams searchParams){
+        return testService.myActiveInitiatedTasks(searchParams);
+    }
+
+    @GetMapping("/myCompletedInitiatedTasks")
+    public List<QuestionnaireInboxItem> myCompletedInitiatedTasks(MyInitiatedSearchParams searchParams){
+        return testService.myCompletedInitiatedTasks(searchParams);
+    }
+
+    @GetMapping("/claimTask")
+    public void claimTask(String taskId){
+        testService.claimTask(taskId);
+    }
+
+    @GetMapping("/getWorkflowStepsFor")
+    public List<WorkflowStep> getWorkflowStepsFor(String processInstanceId){
+        return testService.getWorkflowStepsFor(processInstanceId);
     }
 
 }
